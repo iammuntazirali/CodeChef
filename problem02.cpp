cfe3f16127615e659cc7c40c3d5f428c960c6fa8
Muntazir Ali <muntazirali2018@gmail.com>
2024-12-16 21:02:21 +0530

    Add files via upload

---
+++ problem02.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, m;
+    cin >> n >> m;
+    if(m >= (n + 1) / 2){
+        cout << "NEWBIE";
+    }else{
+        cout << "PRO";
+    }
+}
