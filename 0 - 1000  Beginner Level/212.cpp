70d981df201683f56bcfdd8e6de42362f0f9d165
Muntazir Ali <muntazirali2018@gmail.com>
2024-12-18 22:59:45 +0530

    Chef and Socks

---
+++ 0 - 1000  Beginner Level/212.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,x,y;
+    cin >> a >> x >> y;
+    if(a<x+y){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
