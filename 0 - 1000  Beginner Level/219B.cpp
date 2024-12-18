2d2d79e0efaeb6790ca3b0550133e809ae639068
Muntazir Ali <muntazirali2018@gmail.com>
2024-12-18 23:03:00 +0530

    Glass Prices

---
+++ 0 - 1000  Beginner Level/219B.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int x,y;
+    cin >> x >> y;
+    if(2*x<y){
+        cout << "PLASTIC" << endl;
+    }
+    else{
+        cout << "METAL" << endl;
+    }
+}
