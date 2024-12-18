9b7aff6eaceb26737876106109882303d61a8c6f
Muntazir Ali <muntazirali2018@gmail.com>
2024-12-18 23:05:00 +0530

    The Ides of March

---
+++ 0 - 1000  Beginner Level/231.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    if(n==15){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
