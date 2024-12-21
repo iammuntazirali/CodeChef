ef31f3273dc5870450fbc2d3177bfad13fae3e2c
Muntazir Ali <muntazirali2018@gmail.com>
2024-12-21 23:34:59 +0530

    Summer Time

---
+++ 0 - 1000  Beginner Level/242B.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    cin >> x;
+    if(x>35){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
