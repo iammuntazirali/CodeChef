21670647b58a57f9a39d752255c67194faa79043
Muntazir Ali <muntazirali2018@gmail.com>
2024-12-24 22:20:34 +0530

    Advitiya

---
+++ 0 - 1000  Beginner Level/243.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    if(n>=16 && n<=18){
+        cout << "ADVITIYA" << endl;
+    }
+    else{
+        cout << "WAITING FOR ADVITIYA" << endl;
+    }
+}
