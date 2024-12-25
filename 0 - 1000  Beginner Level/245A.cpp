ca99ae8c1295045b5dc4c4c1a9b270bd2aaf8104
Muntazir Ali <muntazirali2018@gmail.com>
2024-12-25 23:46:38 +0530

    AC Please

---
+++ 0 - 1000  Beginner Level/245A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    if(t>30){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
