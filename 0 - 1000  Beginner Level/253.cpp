f680e458f99ac09eaeeae4d02301a9555331aa2e
Muntazir Ali <muntazirali2018@gmail.com>
2024-12-28 19:02:07 +0530

    Gold Coins 101

---
+++ 0 - 1000  Beginner Level/253.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,x,y;
+    cin >> a >> b >> x >> y;
+    if(x>y){
+        cout << a << endl;
+    }
+    else{
+        cout << b << endl;
+    }
+}
